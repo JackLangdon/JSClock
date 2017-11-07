79c4a9f9ad8bc7f4ee9bc88fa10466da60a2e2c0
John <jlross90@gmail.com>
2017-11-08 00:07:35 +0900

    Updated style

--- script.js
+++ script.js
@@ -14,16 +14,37 @@ function setDate() {
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
   const hours = now.getHours();
-  const hoursDegrees = (((hours/60)*360)+90);
+  const hoursDegrees = (((hours/60)*360)+90+(minutesDegrees/12));
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
-  const allHands = document.querySelectorAll('.hand');
-
   if (secondsDegrees === 90) {
-    allHands.forEach(hand => hand.style.transition = 'none');
+    secondHand.style.transition = 'none';
   } else {
-    allHands.forEach(hand => hand.style.transition = '');
+    secondHand.style.transition = '';
   };
+
+  if (minutesDegrees === 90) {
+    minuteHand.style.transition = 'none';
+  } else {
+    minuteHand.style.transition = '';
+  };
+
+  if (hoursDegrees === 90) {
+    hourHand.style.transition = 'none';
+  } else {
+    hourHand.style.transition = '';
+  };
+
+
+
+  // Shorter code that covers all hands, but removes transition animation for min and hour hands
+
+  // const allHands = document.querySelectorAll('.hand');
+  // if (secondsDegrees === 90) {
+  //   allHands.forEach(hand => hand.style.transition = 'none');
+  // } else {
+  //   allHands.forEach(hand => hand.style.transition = '');
+  // };
 }
 
 setInterval(setDate, 1000);
